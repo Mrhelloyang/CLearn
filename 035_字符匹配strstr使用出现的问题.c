4e06041198f11660ed1408c4c08cd563a15e45eb
hello <75154809+Mrhelloyang@users.noreply.github.com>
2023-03-03 21:15:49 +0800

    Add files via upload

---
+++ 035_字符匹配strstr使用出现的问题.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char str[] = "ajajgejejsduydajfhdjasidjuyaenbfehfeuydfklasdhaslduy";
+	char* t = strstr(str+15, "uy");//这个也不行可以看成*（str+15）
+	 //char* t= strstr( &str[15], "uy");//str在这里就是个地址，直接写str+15就行，如果写成str[15]和*(str+15)等价就成先取str[15]
+	 //然后再取地址的内存，就不是地址了，变成内存了，
+	 /**char* start = str;
+	char *p;
+	int n=0;
+	while (1)
+	{
+		p = strstr(start, "uy");//strstr在start指向的内存里面查找uy出现的首地址，失败就返回NULL
+		if (p != NULL)
+		{
+			start = p + strlen("uy");
+			n++;
+		}
+		else 
+		{
+			break;
+		}
+
+	}
+	printf("%d\n",n); */
+	printf("%s\n", t);
+	return 0;
+}
