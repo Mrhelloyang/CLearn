0e046121ad27b22191e7d379d627312cb68f126b
hello <75154809+Mrhelloyang@users.noreply.github.com>
2023-03-04 10:20:36 +0800

    Update 035_字符匹配strstr使用出现的问题.c

--- 035_字符匹配strstr使用出现的问题.c
+++ 035_字符匹配strstr使用出现的问题.c
@@ -4,8 +4,7 @@ int main()
 {
 	char str[] = "ajajgejejsduydajfhdjasidjuyaenbfehfeuydfklasdhaslduy";
 	char* t = strstr(str+15, "uy");//strstr(地址，查找内容);
-	 //char* t= strstr( &str[15], "uy");//str在这里就是个地址，直接写str+15就行，如果写成str[15]和*(str+15)等价就成先取str[15]
-	 //然后再取地址的内存，就不是地址了，变成内存了，
+	 //char* t= strstr( &str[15], "uy");//str在这里就是个地址，直接写str+15就行，如果写成str[15]和*(str+15)等价就成先取str[15]的内存，就不是地址了，变成内存了，
 	 /**char* start = str;
 	char *p;
 	int n=0;
