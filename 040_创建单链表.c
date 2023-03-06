464c79897768078802fe8afdf8676e29eb2e41eb
hello <75154809+Mrhelloyang@users.noreply.github.com>
2023-03-06 20:40:51 +0800

    Update 040_创建单链表.c

--- 040_创建单链表.c
+++ 040_创建单链表.c
@@ -1,9 +1,11 @@
+/*
 // ListNode 声明如下:
 typedef struct _ListNode
 {
-    int                  value;
-    struct _ListNode* next;
+    int                  value;   
+    struct _ListNode    *next;
 }ListNode;
+*/
 // 参数说明:
 //    a  数组
 //    size 数组a的长度
@@ -11,21 +13,21 @@ typedef struct _ListNode
 //    单链表第一个节点的地址, 如果 size 为 0, 则返回 NULL
 ListNode* create_list(int a[], int size)
 {
-
-   // TODO: 请实现创建单链表, 并返回单链表第一个节点的地址
-    header= (ListNode*)malloc(sizeof(ListNode));
-    ListNode* s, * r = header;
-    if (size == 0)
-    {
-        return NULL;
-    }
-    while (size)
-    {
-        s = (ListNode*)malloc(sizeof(ListNode));
-        r->next = s;
-        size--;
-    }
-    s->next = NULL;
-    return (header - next);
-
+  if(size==0)
+  {
+    return NULL;
+  }
+  ListNode* header ;
+   header = (ListNode*)malloc(sizeof(ListNode));
+  ListNode* p=header ;
+    // TODO: 请实现创建单链表, 并返回单链表第一个节点的地址 
+    // 接收元
+        for (int i = 0; i < size; i++)
+        {
+            int value = a[i];
+            p->next = (ListNode*)malloc(sizeof(ListNode));
+            p->next->value = value;
+            p = p->next;
+        }
+    return header->next;
 }
