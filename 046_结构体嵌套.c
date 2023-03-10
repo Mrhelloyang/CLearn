19f7180593d185a6e004ee9838d652511777a632
hello <75154809+Mrhelloyang@users.noreply.github.com>
2023-03-10 16:18:57 +0800

    Add files via upload

---
+++ 046_结构体嵌套.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+struct Info{
+        char name[50];
+        char sex;
+        int  age;
+};
+struct student{
+        int score;
+        char group[50];
+        struct Info info;
+        //struct info student;错误；不能嵌套本类型；可以嵌套其他类型；
+        //可以嵌套任何类型的指针类型（因为指针的大小是确定的）
+
+};
+
+int main ()
+{
+        struct student s={89,"c++","wangliu",'m',18};
+
+        printf("%d,%s,%s,%c,%d\n",s.score,s.group,s.info.name,s.info.sex,s.info.age);
+
+        return 0;
+}
+
