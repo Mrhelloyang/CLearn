851baf6ad98d744a66c0a0c3717f90aaf57e45e6
hello <75154809+Mrhelloyang@users.noreply.github.com>
2023-03-11 19:30:48 +0800

    Update 054_C语言中的bool类型和typedef.c

--- 054_C语言中的bool类型和typedef.c
+++ 054_C语言中的bool类型和typedef.c
@@ -1,18 +1,23 @@
 include<stdio.h>
-
-enum _bool{//枚举函数；
-
+enum _bool
+{//枚举函数；
         false,true
 };
 typedef enum _bool bool;
-
 int main ()
 {
-
-
                 bool flag;
                 flag =true;
                 flag =false;
-
                 return 0;
 }
+/*typedef的作用；
+1.改名
+typedef int int64;//带分号
+typedef struct student 
+{
+        char tem;
+}stu;//把把结构体struct student改名为stu；
+2.代码移植
+3封装类型
+*/
