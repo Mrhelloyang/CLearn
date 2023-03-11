593861835fe9ea502190dd03cc30067347a72ead
hello <75154809+Mrhelloyang@users.noreply.github.com>
2023-03-11 19:10:50 +0800

    Add files via upload

---
+++ 054_C语言中的bool类型和typedef.c
@@ -0,0 +1,18 @@
+include<stdio.h>
+
+enum _bool{//枚举函数；
+
+        false,true
+};
+typedef enum _bool bool;
+
+int main ()
+{
+
+
+                bool flag;
+                flag =true;
+                flag =false;
+
+                return 0;
+}
