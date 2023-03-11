c79065407655119b3f26e29dc6104f60158cddd6
hello <75154809+Mrhelloyang@users.noreply.github.com>
2023-03-11 15:05:55 +0800

    Update 050_指针易错点.c

--- 050_指针易错点.c
+++ 050_指针易错点.c
@@ -10,7 +10,7 @@ int main()
         strcpy(p,"hello");
         //*p=(char*)"hello";//给p指向的首元素赋值；大小不匹配；err
         int n =sizeof("hello")/sizeof(char);
-        //for(int i=0;i<n-1;i++)
+        //for(int i=0;i<n-1;i++)//这种方法太复杂了，直接用C语言函数strcpy(首地址，内容首地址);
         //      {
         //      *(p+i)=a[i];
         //}
